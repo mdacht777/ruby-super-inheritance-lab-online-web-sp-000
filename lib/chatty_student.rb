393f0e2bc9dfd44f96eb23b5474d92e4fd3122e8
Martin Achtermann <mdacht777@gmail.com>
2020-06-29 01:42:49 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -3,5 +3,5 @@ class ChattyStudent < Student
     puts "Hey there! I'm so excited to learn stuff."
   end
   def raise_hand
-  puts "Pick me!"
+  puts "Pick me!\nPickme!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPickme!\n
 endend
